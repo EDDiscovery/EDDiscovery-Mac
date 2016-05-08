4321b6b90dde374eb74a46502e06939e5bb74d23
Michele Noberasco <michele.noberasco@tiscali.it>
2016-05-08 02:00:05 +0200

    added app update infrastructure

--- EDDiscovery/AppDelegate.m
+++ EDDiscovery/AppDelegate.m
@@ -17,6 +17,16 @@
   [Fabric with:@[[Crashlytics class]]];
 
   [self initializeApplication];
+  
+  NSLog(@"SUFeedURL: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUFeedURL"]);
+  NSLog(@"SUEnableAutomaticChecks: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUEnableAutomaticChecks"]);
+  NSLog(@"SUEnableSystemProfiling: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUEnableSystemProfiling"]);
+  NSLog(@"SUShowReleaseNotes: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUShowReleaseNotes"]);
+  NSLog(@"SUPublicDSAKeyFile: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUPublicDSAKeyFile"]);
+  NSLog(@"SUScheduledCheckInterval: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUScheduledCheckInterval"]);
+  NSLog(@"SUAllowsAutomaticUpdates: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUAllowsAutomaticUpdates"]);
+  NSLog(@"SUBundleName: %@", [[NSUserDefaults standardUserDefaults] objectForKey:@"SUBundleName"]);
+  
 }
 
 #pragma mark -
